099909739881f64850918f026eb581149dee090b
asmaasifchippa786 <asmaasifchippa786@gmail.com>
2022-05-26 13:44:24 +0500

    1st

---
+++ app.js
@@ -0,0 +1,238 @@
+var num1 = 5;
+
+var num2 = 2;
+
+var sum = num1+num2;
+
+var subs = num1-num2;
+
+var multiplication = num1*num2;
+
+var division = num1/num2;
+
+document.write("<h1> MATH EXPRESION </h1><hr/>")
+
+document.write("<h1> sum of 5 and 2 is " + sum + "</h1>");
+
+document.write("<h1> subtracting of 5 and 2 is " + subs + "</h1>");
+
+document.write("<h1> Multiplication of 5 and 2 is " + multiplication + "</h1>");
+
+document.write("<h1> Division of 5 and 2 is " + division + "</h1><hr>");
+
+var write = "Value after variable declaration is : undefined";
+
+document.write("<h1>" + write + "</h1>");
+
+var num3 = 10;
+
+document.write("<h1> Initail Value : " + num3 + "</h1>");
+
+var num4 = num3+1;
+
+document.write("<h1> Value after increment is : " + num4 + "</h1>");
+
+var num5 = num4+7;
+
+document.write("<h1> Value after addition : " + num5 + "</h1>");
+
+var num6 = num5-1;
+
+document.write("<h1> Value after decrement is : " + num6 + "</h1>");
+
+var num7 = num6/num6;
+
+document.write("<h1> The remainder is : " + num7 + "</h1><hr>");
+
+var cost = 600;
+
+document.write("<h1>Total cost to buy 5 tickets to a movie is " + cost*5 + "</h1><hr>");
+
+document.write("<h1>Multiplication Table of 2</h1>");
+
+document.write("<h2>2 x 1 = " + 2*1 + "<h2>");
+document.write("<h2>2 x 2 = " + 2*2 + "<h2>");
+document.write("<h2>2 x 3 = " + 2*3 + "<h2>");
+document.write("<h2>2 x 4 = " + 2*4 + "<h2>");
+document.write("<h2>2 x 5 = " + 2*5 + "<h2>");
+document.write("<h2>2 x 6 = " + 2*6 + "<h2>");
+document.write("<h2>2 x 7 = " + 2*7 + "<h2>");
+document.write("<h2>2 x 8 = " + 2*8 + "<h2>");
+document.write("<h2>2 x 9 = " + 2*9 + "<h2>");
+document.write("<h2>2 x 10 = " + 2*10 + "<h2><hr>");
+
+var c = (9/5*12)+32;
+
+document.write("<h1>12<sup>o</sup>C is " + c + "<sup>o</sup>F</h1>");
+
+var f = (68-32)*5/9;
+
+document.write("<h1>68<sup>o</sup>C is " + f + "<sup>o</sup>C</h1><hr>");
+
+var item1 = 560;
+
+var q1 = 2;
+
+var item2 = 200;
+
+var q2 = 5;
+
+var charges = 150;
+
+var sum1 = item2*q2;
+
+var sum4 = item1*q1
+
+var sum2 = sum1+sum4;
+
+var sum3 = sum2+charges
+
+document.write("<h1>Shopping Cart</h1>")
+
+document.write("<h2>Price of item 1 is " + item1 + "</h2>");
+
+document.write("<h2>Quantity of item 1 is " + q1 + "</h2>");
+
+document.write("<h2>Price of item 2 is " + item2 + "</h2>");
+
+document.write("<h2>Quantity of item 2 is " + q2 + "</h2><br>");
+
+document.write("<h2>Shipping Charges " + charges + "</h1><br>");
+
+document.write("<h2>Total cost of your order is " + sum3 + "PKR</h2><hr>");
+
+var totalMarks = 1200;
+
+var marksObtained = 900;
+
+var pre = marksObtained*100/totalMarks;
+
+document.write("<h1>Marks Sheet</h1>");
+
+document.write("<h1>Total marks : " + totalMarks + "<h1>");
+
+document.write("<h1>Marks obtained : " + marksObtained + "<h1>");
+
+document.write("<h1>Prcentage : " + pre + "%<h1><hr>");
+
+var us = 200.45;
+
+var sum5 = us*10;
+
+var ri = 53.43;
+
+var sum6 = ri*25;
+
+document.write("<h1>Currency in PKR</h1>");
+
+document.write("<h2>10 US Dollar = " + sum5 + " PKR </h2>");
+
+document.write("<h2>25 Saudi Riyal =" + sum6 + " PKR </h2><hr>");
+
+var num8 = 23;
+
+var sum7 = 23+5;
+
+var multi = sum7*10;
+
+var div = multi/2;
+
+document.write("<h1>I take " + num8 + " Number</h1>");
+
+document.write("<h1>after add with 5 the answer is " + sum7 + " Number </h1>");
+
+document.write("<h1>after multiplication with 10 the answer is " + multi + " Number </h1>");
+
+document.write("<h1>after Division with 2 the answer is " + div + " Number </h1><hr>");
+
+document.write("<h1>The Age Calculator</h1>");
+
+var cyear = 2022;
+
+var byear = 2009;
+
+var old = cyear-byear;
+
+var old1 = 2021-byear;
+
+document.write("<h2>Current Year " + cyear + " </h2>");
+
+document.write("<h2>Birtg Year " + byear + " </h2>");
+
+document.write("<h2>They Are either " + old +  " or " + old1 + "Years old" +   " </h2><hr>");
+
+var radius = 12;
+
+document.write("<h1>THe Geometrizer</h1>");
+
+var circum = 2*3.142*radius;
+
+document.write("<h2>The radius of a circle : " + radius + "</h2>");
+
+document.write("<h2>The Circumference is : " + circum + "</h2>");
+
+var area = 3.142*12*12;
+
+document.write("<h2>The arae is : " + area + "</h2><hr>");
+
+document.write("<h2>Result:</h2>");
+
+var num9 = 10;
+
+var num10 = 1+num9;
+
+var num11 = num10+1;
+
+var num12 = 1-num11;
+
+var num13 = num10-1;
+
+document.write("<h2>The Value of a is: " + num9 + '</h2><hr style="border-style:dotted;">');
+
+document.write("<h2>The value of ++a is: " + num10 + "</h2>") ;
+
+document.write("<h2>Now the value is: " + num10 + "</h2><br>");
+
+document.write("<h2>The value of a++ is: " + num11 + "<h2>");
+
+document.write("<h2>Now the value of is: " + num11 + "</h2><br>");
+
+document.write("<h2>The value of --a is: " + num12 + "</h2>");
+
+document.write("<h2>Now the value of a is: " + num12 + "</h2><br>");
+
+document.write("<h2>The value of a-- is: " + num13 + "</h2>");
+
+document.write("<h2>Now the value of a is: " + num13 + "</h2><hr>");
+
+var a = 14;
+var b = 80;
+var result = --a - --b + ++b + b--;
+
+document.write("<h2>a is " + a + "</h2>");
+
+document.write("<h2>b is " + b + "</h2>");
+
+document.write("<h2>result is " + result + "</h2><hr>");
+
+var snaks = "Lays";
+
+var age = 12;
+
+var maxage = 90;
+
+var amount = 3;
+
+var sum8 =(90-12)*3;
+
+document.write("<h1>THe Lifetime Supply Calculator</h1><br>");
+
+document.write("<h2>Favorite Snack: " + snaks + "</h2>");
+
+document.write("<h2>Current Age : " + age + "</h2>");
+
+document.write("<h2>Estimated Maximum Age : " + maxage + "</h2>");
+
+document.write("<h2> Amount of Snacks per day : " + amount + "</h2>");
+
+document.write("<h2>You will need " + sum8 + " " + snaks + " to last you until the ripe old age of " + maxage + "</h2>")
